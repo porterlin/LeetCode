6aaee0c9e7cb41c741b8468744c8333cb850dfc5
porterlin <porter0418@gmail.com>
2025-05-22 03:35:23 +0800

    add 1292, 221

---
+++ prefix-sum/221.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> sum(m + 1, vector<int>(n + 1));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                sum[i + 1][j + 1] = sum[i + 1][j] + sum[i][j + 1] - sum[i][j] + matrix[i][j] - '0';
+            }
+        }
+
+        auto check = [&](int side) -> bool {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i + 1 - side < 0 || j + 1 - side < 0)
+                        continue;
+                    int val = sum[i + 1][j + 1] - sum[i + 1][j + 1 - side] - sum[i + 1 - side][j + 1] + sum[i + 1 - side][j + 1 - side];
+                    if (val == side * side)
+                        return true;
+                }
+            }
+            return false;
+        };
+
+        int left = 1, right = min(m, n);
+        int mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (check(mid))
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return right * right;
+    }
+};
